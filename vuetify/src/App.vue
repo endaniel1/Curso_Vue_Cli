a6799800f261281ed4f1707211de689502baa765
Robert Gonzalez <robert_gon97@hotmail.com>
2020-06-28 10:24:25 -0400

    Agregando vue a material-desind

---
+++ vuetify/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-toolbar app class="info" dark >
+      <v-app-bar-nav-icon @click="menu=!menu"></v-app-bar-nav-icon>
+      <v-toolbar-title>Titulo</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn class="success">Login</v-btn>
+      <v-btn class="error">Log out</v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer app v-model="menu">
+      <v-list-item two-line :class="miniVariant && 'px-0'">
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/men/81.jpg">
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Programador</v-list-item-title>
+          <v-list-item-subtitle>Novato</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    menu:false
+  }),
+};
+</script>
---
+++ vuetify/src/views/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+  	<v-layout>
+  		<v-flex>
+  			<v-btn color="success" >button</v-btn>
+  			<v-btn dark color="blue">button</v-btn>
+  			<v-btn text color="primary">button</v-btn>  			
+  			<v-btn color="warning">button</v-btn>
+  			<v-btn large color="warning">button</v-btn>
+  			<v-btn small color="warning">button</v-btn>
+
+  			<v-btn dark depressed color="red">button</v-btn>
+
+  			<v-btn flat icon color="warning">
+  				<v-icon>mdi-heart</v-icon>
+  			</v-btn>
+  			<v-btn :large="largo" color="warning">
+  				<v-icon>mdi-heart</v-icon>
+  			</v-btn>
+  			<v-btn @click="largo^=true;">
+  				click
+  			</v-btn>
+  		</v-flex>
+  	</v-layout>
+  </v-container>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+				largo:true
+			}
+		}
+	}
+</script>
